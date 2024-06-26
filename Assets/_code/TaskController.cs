2adbcf644c2ec1e5d4e22308ec6b1b2dddb7ef35
Maskayop <alexei.erepov@gmail.com>
2024-06-26 20:11:07 +0300

    Добавил подарки и счётчик стоимости подарков

--- Assets/_code/TaskController.cs
+++ Assets/_code/TaskController.cs
@@ -1,7 +1,12 @@
+using TMPro;
 using UnityEngine;
 
 public class TaskController : MonoBehaviour
 {
     public CanvasGroup canvasGroup;
     public GameObject rewardStarCheckmark;
+    public GameObject rewardStar;
+    public GameObject giftCost;
+    public TextMeshProUGUI giftCostAmountText;
+    public bool isGift = false;
 }
--- Assets/_code/TasksManager.cs
+++ Assets/_code/TasksManager.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class TasksManager : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI starsAmountText;
+    [SerializeField] TextMeshProUGUI giftsCostAmountText;
 
     TaskController[] taskControllers;
     List<TaskController> nonActiveTasks = new List<TaskController>();
 
     int starsAmount = 0;
+    int giftsCostAmount = 0;
 
     void Awake()
     {
@@ -25,6 +27,7 @@ public class TasksManager : MonoBehaviour
     void Start()
     {
         CountStars();
+        CountGiftsCost();
     }
 
     public void ShowNonActiveTasks()
@@ -43,10 +46,26 @@ public class TasksManager : MonoBehaviour
     {
         for (int i = 0; i < taskControllers.Length; i++)
         {
-            if (taskControllers[i].canvasGroup.alpha == 1 && taskControllers[i].rewardStarCheckmark.activeSelf)
+            if (taskControllers[i].canvasGroup.alpha == 1 && taskControllers[i].rewardStarCheckmark.activeSelf && !taskControllers[i].isGift)
                 starsAmount++;
         }
 
         starsAmountText.text = starsAmount.ToString();
+    }
+
+    void CountGiftsCost()
+    {
+        for (int i = 0; i < taskControllers.Length; i++)
+        {
+            if (taskControllers[i].isGift)
+            {
+                int value;
+                int.TryParse(taskControllers[i].giftCostAmountText.text, out value);
+
+                giftsCostAmount += value;
+            }
+        }
+
+        giftsCostAmountText.text = giftsCostAmount.ToString();
     }
 }
